fad588cd79090fb81f2be9396c19f54d7cba5b90
ahmedseragcodes <ahmedserag1@gmail.com>
2021-03-23 17:23:06 -0400

    wrote functions for handling input form changes and submission

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+//TECH IMPORTS
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
+//COMPONENT IMPORTS
+import { fetchFriends } from "./components/actions";
+import axiosWithAuth from "./components/axiosWithAuth";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+
+const { formValues, setFormValues }=useState({
+  username: "",
+  password: "",
+})
+
+//HANDLES LOGIN FORM INPUTS 
+  const handleLoginFormChange = (event)=>{
+
+    const { name, value, checked, type }=event.target;
+
+    const valueToUse = type === "checkbox" ? checked : value
+
+    setFormValues({
+      ...formValues,
+      [name]: valueToUse
+    })
+  }
+
+//HANDLES LOGIN FORM SUBMISSION 
+  const handleLoginFormSubmit = (event)=>{
+    event.preventDefault();
+    axiosWithAuth()
+    axios.post("/api/login", formValues)
+    .then((res)=>{
+      console.log("SUCCESSFULLY POSTED LOGIN CREDS", res);
+      // localStorage.setItem("token", res.data);
+    })
+    .catch((err)=>{
+      console.log("FAILED TO POST LOGIN CREDS", err);
+    })
+  }
+
+  //BEGIN FUNCTIONAL COMPONENT RETURN 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="frontPageCatchAll">
+     <h1>Lambdalorians Club</h1>
+   </div>
   );
 }
 
-export default App;
+
+//MAP STATE / REDUX LOGIC START 
+const mapStateToProps = (state) => {
+  return ({
+    friends: state.friends,
+    error: state.error,
+    isLoading: state.isLoading,
+  })
+}
+
+
+export default connect(mapStateToProps, { fetchFriends })(App);
